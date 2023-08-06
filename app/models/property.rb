fe64077a19230ce7c31ea068952dd5ddcb7d618d
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2023-08-06 13:43:39 +0100

    Add: Profile

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,7 +1,10 @@
 class Property < ApplicationRecord
   belongs_to :landlord, class_name: 'User'
 
+  validates :property_name, presence: true
   validates :address, presence: true
   validates :rent_amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :property_type, presence: true
+
+  has_many_attached :images
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :properties
-  resources :users
+  namespace :users do
+    resource :profile, only: [:show, :edit, :update]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
